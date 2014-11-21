a9f32314f5c3d7397d15e1ac4d6f97aed74589e5
Joan Guillén i Pelegay <jguille2@xtec.cat>
2014-11-21 23:26:08 +0100

    Release 2.0 - First version at github

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-IWcat2
-======
+IWcat2 theme
+============
+IWcat2 theme 2.0 **for Zikula 1.3.x**
 
-zikula theme
+  - Based on IWxtec2 theme from [Agora](https://github.com/projectestac/agora) project
+  - Used on [Sirius](apliense.xtec.cat/sirius) project
+
+Changelog
+---------
+
+### Master branch
+
+
+### IWcat2 theme 2.0 (2014/11/21)
+
+Init version at github
---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+$themeversion['name'] = 'IWcat2';
+$themeversion['displayname'] = __('Catàleg de formació - 2');
+$themeversion['description'] = __('Adaptació del theme IWxtec2 per al catàleg de formació');
+$themeversion['regid'] = '0';
+$themeversion['version'] = '2.0';
+$themeversion['official'] = '0';
+$themeversion['author'] = 'Joan Guillén';
+$themeversion['contact'] = 'enfocat@gmail.com';
+$themeversion['admin'] = 1;
+$themeversion['user'] = 1;
+$themeversion['system'] = 1;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'GNU/GPL';
+$themeversion['xhtml'] = true;
